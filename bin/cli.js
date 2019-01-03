c9a2f19ce223a6ec7a62c6c71bf79197c55bfc71
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-01-03 16:27:04 +0100

    Dont do unrequired transformations

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-const { reduce } = require("@asd14/m")
-
 // First args will always be the node path followed by interpreted file
 const params = require("minimist")(process.argv.slice(2), {
   alias: {
@@ -18,9 +16,9 @@ const params = require("minimist")(process.argv.slice(2), {
 })
 
 require("../dist").default({
-  require: reduce((acc, item) => [...acc, "-r", item], [])(
-    Array.isArray(params.require) ? params.require : [params.require]
-  ),
+  requireModules: Array.isArray(params.require)
+    ? params.require
+    : [params.require],
   path: params.path,
   pattern: params.glob,
 })
