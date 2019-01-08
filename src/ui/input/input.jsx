cb77dbfb6226e781f23373a7020b50ee368a5e2d
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-01-08 18:06:53 +0100

    UI input component. Wrapper over blessed textbox.

---
+++ src/ui/input/input.jsx
@@ -0,0 +1,115 @@
+// @flow
+
+import * as React from "react"
+import { isEmpty } from "@asd14/m"
+
+import { labelStyle, inputStyle, wrapperStyle } from "./input.style"
+
+type PropType = {|
+  label: string,
+  value: string,
+  top: number | string,
+  left: number | string,
+  width: number | string,
+  onChange: (value: string) => void,
+  onSubmit: (value: string) => void,
+  onCancel: (value: string) => void,
+  hasFocusOnMount: boolean,
+|}
+
+export class UIInput extends React.PureComponent<PropType> {
+  static defaultProps = {
+    label: "",
+    top: "center",
+    left: "center",
+    width: "50%",
+    hasFocusOnMount: true,
+  }
+
+  /**
+   * This function will be called only once in the whole life-cycle of a given
+   * component and it being called signalizes that the component and all its
+   * sub-components rendered properly.
+   *
+   * DO
+   *  - cause side effects (AJAX calls etc.)
+   *
+   * DON'T
+   *  - call this.setState as it will result in a re-render
+   */
+  componentDidMount = () => {
+    const { value, hasFocusOnMount } = this.props
+
+    this.refFilterInput.on("keypress", (code, key) => {
+      if (!isEmpty(code) && /[a-zA-Z0-9 \-=\.]/.test(code)) {
+        this.handleInputAdd(code)
+      }
+      if (key.full === "backspace") {
+        this.handleInputPop()
+      }
+    })
+
+    this.refFilterInput.setValue(value)
+    hasFocusOnMount && this.refFilterInput.focus()
+  }
+
+  /**
+   * Examine this.props and this.state and return a single React element. This
+   * element can be either a representation of a native DOM component, such as
+   * <div />, or another composite component that you've defined yourself.
+   *
+   * @return {React.Node}
+   */
+  render = (): React.Node => {
+    const { value, label, top, left, width, onSubmit, onCancel } = this.props
+
+    return (
+      <element
+        ref={this.linkRefFilterWraper}
+        keyable={true}
+        class={wrapperStyle}
+        width={width}
+        top={top}
+        left={left}
+        height={3}>
+        {!isEmpty(label) && (
+          <box class={labelStyle} width={label.length} content={label} />
+        )}
+        <textbox
+          ref={this.linkRefFilterInput}
+          class={inputStyle}
+          left={label.length + 1}
+          inputOnFocus={true}
+          width={`100%-${label.length + 4}`}
+          content={value}
+          onSubmit={onSubmit}
+          onCancel={onCancel}
+        />
+      </element>
+    )
+  }
+
+  handleInputAdd = (code: string) => {
+    const { value, onChange } = this.props
+
+    onChange(`${value}${code}`)
+  }
+
+  handleInputPop = () => {
+    const { value, onChange } = this.props
+
+    onChange(value.substr(0, value.length - 1))
+  }
+
+  linkRefFilterInput = (ref: Object) => {
+    this.refFilterInput = ref
+  }
+
+  linkRefFilterWraper = (ref: Object) => {
+    this.refFilterWrapper = ref
+  }
+
+  refFilterInput = {}
+
+  refFilterWrapper = {}
+}
---
+++ src/ui/input/input.style.js
@@ -0,0 +1,40 @@
+export const wrapperStyle = {
+  padding: {
+    top: 0,
+    left: 1,
+    bottom: 0,
+    right: 0,
+  },
+  border: {
+    type: "line",
+    fg: "white",
+    bold: true,
+  },
+}
+
+export const inputStyle = {
+  tags: false,
+  height: 1,
+  padding: {
+    top: 0,
+    left: 1,
+    bottom: 0,
+    right: 0,
+  },
+  style: {
+    fg: "white",
+  },
+}
+
+export const labelStyle = {
+  height: 1,
+  padding: {
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  style: {
+    fg: "white",
+  },
+}
