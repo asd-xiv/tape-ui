ca5e612c579e893cfb6971f1351dc95570323bd5
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-01-18 02:49:04 +0100

    Hide menu when filter input is visible

--- src/app/app.view.jsx
+++ src/app/app.view.jsx
@@ -2,14 +2,13 @@
 
 import * as React from "react"
 import {
-  pipe,
-  filter,
   map,
   findBy,
   replace,
   hasWith,
+  filter,
+  contains,
   isEmpty,
-  count,
 } from "@asd14/m"
 
 import { UIFile } from "../ui/file/file"
@@ -18,22 +17,18 @@ import { UIDebug } from "../ui/debug/debug"
 import { UIMenu } from "../ui/menu/menu"
 import { UIInput } from "../ui/input/input"
 
-import type {
-  TestFilesType,
-  AppStateType,
-  AppActionsType,
-} from "./app.container"
-import type { UIListItemType } from "../ui/list/list"
+import type { TestFile, AppState, AppActions } from "./app.container"
+import type { ListItem } from "../ui/list/list__item"
 
-type PropsType = {
+type Props = {
+  actions: AppActions,
   name: string,
-  version: string,
   projectVersion: string,
-  store: AppStateType,
-  actions: AppActionsType,
+  store: AppState,
+  version: string,
 }
 
-class AppView extends React.Component<PropsType> {
+class AppView extends React.Component<Props> {
   /**
    * Examine this.props and this.state and return a single React element. This
    * element can be either a representation of a native DOM component, such as
@@ -46,71 +41,54 @@ class AppView extends React.Component<PropsType> {
       name,
       version,
       store: {
-        filesSelectedPath,
         files,
-        filesFilter,
+        fileSelectedPath,
+        filterQuery,
         runArgs,
         isDebugVisible,
         isFilterVisible,
       },
       actions: {
-        xHandleFileRun,
+        xHandleChangeSelected,
+        xHandleRun,
         xHandleFilterChange,
         xHandleFilterSubmit,
         xHandleFilterClose,
       },
     } = this.props
-    const filesSelected: TestFilesType =
-      findBy({ path: filesSelectedPath })(files) ?? {}
-    const filesFilteredCount = isEmpty(filesFilter)
-      ? files.length
-      : count(item => item.path.indexOf(filesFilter) !== -1)(files)
+    const filesSelected: TestFile =
+      findBy({ path: fileSelectedPath })(files) ?? {}
+    const filesFiltered = filter(item => contains(filterQuery)(item.path))(
+      files
+    )
 
     return [
       <UIList
         key="app-files-list"
         selectedId={filesSelected.path}
         label={
-          isEmpty(filesFilter)
+          isEmpty(filterQuery)
             ? `${files.length} files`
-            : `${filesFilteredCount}/${files.length} files`
+            : `${filesFiltered.length}/${files.length} files`
         }
         top="0"
         left="0"
-        width="30%+1"
-        height={isFilterVisible ? "100%-3" : "100%-1"}
-        items={pipe(
-          filter(
-            (item: TestFilesType): boolean =>
-              item.path.indexOf(filesFilter) !== -1
-          ),
-          map(
-            (item: TestFilesType): UIListItemType => ({
-              id: item.path,
-              label: replace(filesFilter, `{bold}${filesFilter}{/bold}`)(
-                item.name
-              ),
-              code: item.code,
-              isLoading: item.isLoading,
-            })
-          )
-        )(files)}
+        width="30%"
+        height="100%-1"
+        items={map(
+          (item: TestFile): ListItem => ({
+            id: item.path,
+            label: replace(filterQuery, `{bold}${filterQuery}{/bold}`)(
+              item.name
+            ),
+            code: item.code,
+            isLoading: item.isLoading,
+          })
+        )(filesFiltered)}
         isLoading={hasWith({ isLoading: true })(files)}
-        onSelect={xHandleFileRun}
+        onChange={xHandleChangeSelected}
+        onSelect={xHandleRun}
       />,
-      isFilterVisible ? (
-        <UIInput
-          key="files-selected-filter-input"
-          top="100%-4"
-          width="30%+1"
-          left="0"
-          label="Filter:"
-          value={filesFilter}
-          onChange={xHandleFilterChange}
-          onSubmit={xHandleFilterSubmit}
-          onCancel={xHandleFilterClose}
-        />
-      ) : null,
       <UIFile
         key="app-files-selected"
         label={filesSelected.name}
@@ -118,7 +96,7 @@ class AppView extends React.Component<PropsType> {
         top="0"
         left="30%"
         width="70%"
-        height={isDebugVisible ? "70%+1" : "100%-1"}
+        height={isDebugVisible ? "70%" : "100%-1"}
         content={filesSelected.content}
         code={filesSelected.code}
         signal={filesSelected.signal}
@@ -140,13 +118,27 @@ class AppView extends React.Component<PropsType> {
           height="30%"
         />
       ) : null,
-      <UIMenu
-        key="app-menu"
-        name={name}
-        version={version}
-        isDebugVisible={isDebugVisible}
-        isFilterVisible={isFilterVisible}
-      />,
+      isFilterVisible ? (
+        <UIInput
+          key="files-selected-filter-input"
+          top="100%-1"
+          width="30%"
+          left="0"
+          label="Filter:"
+          value={filterQuery}
+          onChange={xHandleFilterChange}
+          onSubmit={xHandleFilterSubmit}
+          onCancel={xHandleFilterClose}
+        />
+      ) : (
+        <UIMenu
+          key="app-menu"
+          name={name}
+          version={version}
+          isDebugVisible={isDebugVisible}
+          isFilterVisible={isFilterVisible}
+        />
+      ),
     ]
   }
 }
