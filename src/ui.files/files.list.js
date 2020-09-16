befa799cb9aaed1f756c328ef0e24d46bb17d787
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2020-09-16 15:32:39 +0200

    feat: Add FileList.readOne to scan for file dependencies

--- src/ui.files/files.list.js
+++ src/ui.files/files.list.js
@@ -1,27 +1,35 @@
-const { flatten, distinct, split, last, map, pipe } = require("m.xyz")
-const { buildList } = require("just-a-list.redux")
-const { sep } = require("path")
 const glob = require("glob")
+const dependencyTree = require("dependency-tree")
+const { sep } = require("path")
+const { buildList } = require("just-a-list.redux")
+const { flatten, distinct, split, last, map, pipe } = require("m.xyz")
 
 const FileList = buildList({
   name: "FILES",
 
   // @signature (fileGlob: String[]): Object[]
-  read: fileGlob => {
-    return pipe(
-      map(item => glob.sync(item, { absolute: true })),
-      flatten,
-      distinct,
-      map(item => {
-        return {
-          id: item,
-          name: pipe(split(sep), last)(item),
-          isRunning: false,
-          code: null,
-        }
-      })
-    )(fileGlob)
-  },
+  read: pipe(
+    map(item => glob.sync(item, { absolute: true })),
+    flatten,
+    distinct,
+    map(item => {
+      return {
+        id: item,
+        name: pipe(split(sep), last)(item),
+        isRunning: false,
+        code: null,
+      }
+    })
+  ),
+
+  readOne: id => ({
+    id,
+    shouldRunOnChange: dependencyTree.toList({
+      filename: id,
+      directory: __dirname,
+      filter: path => path.indexOf("node_modules") === -1,
+    }),
+  }),
 
   update: (id, data) => ({
     id,
